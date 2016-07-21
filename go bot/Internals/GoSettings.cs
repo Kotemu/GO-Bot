cffb9de3d55f74637449428ece85f383aa72c991
Anthony Puppo <anthonyosx@gmail.com>
2016-07-21 10:43:35 -0400

    v0.0.2

--- go bot/Internals/GoSettings.cs
+++ go bot/Internals/GoSettings.cs
@@ -7,8 +7,8 @@ namespace GO_Bot.Internals {
 	internal class GoSettings : ISettings {
 
 		public AuthType AuthType { get; set; } = AuthType.Ptc;
-		public double DefaultLatitude { get; set; }
-		public double DefaultLongitude { get; set; }
+		public double DefaultLatitude { get; set; } = 52.379189;
+		public double DefaultLongitude { get; set; } = 4.899431;
 		public string GoogleRefreshToken { get; set; } = String.Empty;
 		public string PtcPassword { get; set; } = String.Empty;
 		public string PtcUsername { get; set; } = String.Empty;
--- go bot/Views/MainWindow.xaml.cs
+++ go bot/Views/MainWindow.xaml.cs
@@ -1,18 +1,17 @@
-﻿using AllEnum;
-using GO_Bot.Internals;
+﻿using GO_Bot.Internals;
 using GO_Bot.Models;
 using NLog;
 using PokemonGo.RocketAPI;
-using PokemonGo.RocketAPI.Extensions;
-using PokemonGo.RocketAPI.GeneratedCode;
+using PokemonGo.RocketAPI.Enums;
+using PokemonGo.RocketAPI.Exceptions;
+using PokemonGo.RocketAPI.Logic;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using Logger = NLog.Logger;
 
 namespace GO_Bot.Views {
 
@@ -36,27 +35,26 @@ namespace GO_Bot.Views {
 			backgroundTask = new BackgroundTask(() => {
 				bool b = Task.Run(async () => {
 					GoSettings goSettings = new GoSettings();
-					var client = new Client(goSettings);
 
+					logger.Info("Setting up...");
+					goSettings.AuthType = AuthType.Ptc;
 					await txtUsername.SafeAccessAsync((t) => goSettings.PtcUsername = t.Text);
 					await txtPassword.SafeAccessAsync((t) => goSettings.PtcPassword = t.Password);
 					await txtLatitude.SafeAccessAsync((t) => goSettings.DefaultLatitude = Convert.ToDouble(t.Text));
 					await txtLongitude.SafeAccessAsync((t) => goSettings.DefaultLongitude = Convert.ToDouble(t.Text));
-					logger.Info("Logging in...");
-					await client.DoPtcLogin(goSettings.PtcUsername, goSettings.PtcPassword);
-					logger.Info("Setting up...");
-					await client.SetServer();
 
-					var profile = await client.GetProfile();
-					var settings = await client.GetSettings();
-					var mapObjects = await client.GetMapObjects();
-					var inventory = await client.GetInventory();
-					var pokemons = inventory.InventoryDelta.InventoryItems.Select(i => i.InventoryItemData?.Pokemon).Where(p => p != null && p?.PokemonId > 0);
-
-					logger.Info("Beginning to farm...");
-					await ExecuteFarmingPokestopsAndPokemons(client);
-					logger.Info("Farmed everything around your default coordinates!");
-
+					try {
+						logger.Info("Beginning to farm...");
+						PokemonGo.RocketAPI.Logger.SetLogger(new PokemonGo.RocketAPI.Logging.ConsoleLogger(PokemonGo.RocketAPI.LogLevel.Info));
+						await new Logic(goSettings).Execute();
+					} catch (Exception e) {
+						if (e is PtcOfflineException) {
+							logger.Info("Could not log in (login servers may be down or invalid credentials)");
+						} else {
+							logger.Error(e);
+						}
+					}
+					
 					return true;
 				}).Result;
 			});
@@ -113,7 +111,7 @@ namespace GO_Bot.Views {
 			GeneralSettings settings = Settings.GeneralSettings;
 			listBoxWriter = new ListBoxWriter(lbLog);
 
-			Title = "GO Bot v0.0.1 [BETA]";
+			Title = "GO Bot v0.0.2 [BETA]";
 			Console.SetOut(listBoxWriter);
 			Console.SetError(listBoxWriter);
 			
@@ -141,57 +139,7 @@ namespace GO_Bot.Views {
 			Regex regex = new Regex("[^0-9]+");
 			e.Handled = regex.IsMatch(e.Text);
 		}
-
-		private static async Task ExecuteFarmingPokestopsAndPokemons(Client client) {
-			var mapObjects = await client.GetMapObjects();
-
-			var pokeStops = mapObjects.MapCells.SelectMany(i => i.Forts).Where(i => i.Type == FortType.Checkpoint && i.CooldownCompleteTimestampMs < DateTime.UtcNow.ToUnixTime());
-
-			foreach (var pokeStop in pokeStops) {
-				var update = await client.UpdatePlayerLocation(pokeStop.Latitude, pokeStop.Longitude);
-				var fortInfo = await client.GetFort(pokeStop.Id, pokeStop.Latitude, pokeStop.Longitude);
-				var fortSearch = await client.SearchFort(pokeStop.Id, pokeStop.Latitude, pokeStop.Longitude);
-
-				logger.Info($"[{DateTime.Now.ToString("HH:mm:ss")}] Farmed XP: {fortSearch.ExperienceAwarded}, Gems: { fortSearch.GemsAwarded}, Eggs: {fortSearch.PokemonDataEgg} Items: {GetFriendlyItemsString(fortSearch.ItemsAwarded)}");
-
-				await Task.Delay(15000);
-				await ExecuteCatchAllNearbyPokemons(client);
-			}
-		}
-
-		private static async Task ExecuteCatchAllNearbyPokemons(Client client) {
-			var mapObjects = await client.GetMapObjects();
-
-			var pokemons = mapObjects.MapCells.SelectMany(i => i.CatchablePokemons);
-
-			foreach (var pokemon in pokemons) {
-				var update = await client.UpdatePlayerLocation(pokemon.Latitude, pokemon.Longitude);
-				var encounterPokemonRespone = await client.EncounterPokemon(pokemon.EncounterId, pokemon.SpawnpointId);
-
-				CatchPokemonResponse caughtPokemonResponse;
-				do {
-					caughtPokemonResponse = await client.CatchPokemon(pokemon.EncounterId, pokemon.SpawnpointId, pokemon.Latitude, pokemon.Longitude, MiscEnums.Item.ITEM_POKE_BALL); //note: reverted from settings because this should not be part of settings but part of logic
-				}
-				while (caughtPokemonResponse.Status == CatchPokemonResponse.Types.CatchStatus.CatchMissed);
-
-				logger.Info(caughtPokemonResponse.Status == CatchPokemonResponse.Types.CatchStatus.CatchSuccess ? $"[{DateTime.Now.ToString("HH:mm:ss")}] We caught a {pokemon.PokemonId}" : $"[{DateTime.Now.ToString("HH:mm:ss")}] {pokemon.PokemonId} got away..");
-				await Task.Delay(5000);
-			}
-		}
-
-		private static string GetFriendlyItemsString(IEnumerable<FortSearchResponse.Types.ItemAward> items) {
-			var enumerable = items as IList<FortSearchResponse.Types.ItemAward> ?? items.ToList();
-
-			if (!enumerable.Any())
-				return string.Empty;
-
-			return
-				enumerable.GroupBy(i => i.ItemId)
-						  .Select(kvp => new { ItemName = kvp.Key.ToString(), Amount = kvp.Sum(x => x.ItemCount) })
-						  .Select(y => $"{y.Amount} x {y.ItemName}")
-						  .Aggregate((a, b) => $"{a}, {b}");
-		}
-
+		
 	}
 
 }
